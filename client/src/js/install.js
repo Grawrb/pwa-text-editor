e52233714d9c3aa5222968626be7997ffe4317d5
Grawrb <robmaxfield22@gmail.com>
2024-08-20 19:43:43 -0600

    Added necessary code to client side to complete logic for database.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Event handler for the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    const isAppInstalled = localStorage.getItem('isAppInstalled');
+    if (isAppInstalled) {
+        butInstall.classList.toggle('hidden', true);
+    }});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    if (!promptEvent) {
+     return;
+    }
+    // Show the install prompt
+    promptEvent.prompt();
+    
+    window.deferredPrompt = null;
+    // Toggles the 'hidden' class on for the install button container
+    butInstall.classList.toggle('hidden', true);});
+
+// Handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('Jate was installed.', event);
+    // Toggles the 'hidden' class on for the install button container
+    butInstall.classList.toggle('hidden', true);
+    window.deferredPrompt = null;});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,18 +11,64 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      header: './src/js/header.js',
+      editor: './src/js/editor.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Jate',
+        short_name: 'Jate',
+        description: 'Ofline text editor',
+        background_color: '#01579b',
+        theme_color: '#01579b',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
         
       ],
     },
